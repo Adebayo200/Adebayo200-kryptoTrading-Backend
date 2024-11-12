e844b242282c176f0a46fccd7e457f1cd9bac924
Ade <tik.eskanor@gmail.com>
2024-11-12 12:12:35 +0100

    Delete user Api integration

--- Api/delete_user.php
+++ Api/delete_user.php
@@ -3,7 +3,7 @@
  header("Access-Control-Allow-Origin: *");
  header("Access-Control-Allow-Method: GET");
  header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Method,Authorization, X-Requested-With"); 
- header("Content-Type: application/json");
+//  header("Content-Type: application/json");
 
  require_once("../auto_loader.php");
     
