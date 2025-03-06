7beeef391f1b1753e09c8cf1ddb74d443b232c44
Ade <tik.eskanor@gmail.com>
2025-03-06 15:03:57 +0100

    Push commit

--- Api/Mail_sender/config.php
+++ Api/Mail_sender/config.php
@@ -6,16 +6,16 @@ define('MAILHOST',"smtp.gmail.com");
 define('USERNAME',"kryptotrade.kt@gmail.com");
 
 // App password from google
-define('PASSWORD',"mlsw gzxx uuot ezwu");
+define('PASSWORD',"mlswgzxxuuotezwu");
 
 // Sender
-define('SEND_FROM',"kryptotrade@kryptotradingbackend.com.ng");
+define('SEND_FROM',"kryptotrade.kt@gmail.com");
 
 // Sender name
 define('SEND_FROM_NAME',"Kryptotrading");
 
 // Reply to email
-define('REPLY_TO',"kryptotrade@kryptotradingbackend.com.ng");
+define('REPLY_TO',"kryptotrade.kt@gmail.com");
 
 // Reply to email name
 define('REPLY_TO_NAME',"Kryptotrading");
